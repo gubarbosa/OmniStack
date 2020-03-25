59c42816f972234afdb225603087d1f046acd044
gubarbosa <gugabarbosa.bsi@gmail.com>
2020-03-24 22:57:33 -0300

    organizando as responsividades

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document
+        .querySelector("button.fat")
+        .addEventListener("click", function(){
+            document.querySelector("#footer")
+                .classList
+                .toggle("hide")
+        })
