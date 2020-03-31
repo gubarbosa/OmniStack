8a52f44a351b89084adb4a5adcddb435ed43583d
gubarbosa <gugabarbosa.bsi@gmail.com>
2020-03-31 05:08:27 -0300

    criando ideias.html

--- scripts.js
+++ scripts.js
@@ -2,5 +2,14 @@ function onOff() {
     document.querySelector("#modal")
         .classList
         .toggle("hide")
-}
 
+    document
+        .querySelector("body")
+        .classList
+        .toggle("hideScroll")
+    
+    document
+        .querySelector("#modal")
+        .classList
+        .toggle("addScroll")
+}
