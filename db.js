911a528456c5eeaeaab4899cf5a849cc099814de
gubarbosa <gugabarbosa.bsi@gmail.com>
2020-04-20 16:56:28 -0300

    projeto casa criativa finalizado

--- db.js
+++ db.js
@@ -16,24 +16,24 @@ db.serialize(function() {
     `)
 
 
-    // Inserir dados na tabela
-    // const query = `
-    //     INSERT INTO ideas(
-    //         image,
-    //         title,
-    //         category,
-    //         description,
-    //         link
-    //     ) VALUES (?,?,?,?,?);
-    // `
+    ;//Inserir dados na tabela
+    const query = `
+        INSERT INTO ideas(
+            image,
+            title,
+            category,
+            description,
+            link
+        ) VALUES (?,?,?,?,?);
+    `
 
-    // const values = [
-    //     "https://image.flaticon.com/icons/svg/2729/2729007.svg",
-    //     "Cursos de Programação",
-    //     "Estudo",
-    //     "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus quis est totam itaque earum",
-    //     "https://rocketseat.com.br"
-    // ]
+    const values = [
+        "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+        "Cursos de Programação",
+        "Estudo",
+        "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ducimus quis est totam itaque earum",
+        "https://rocketseat.com.br"
+    ]
 
     // db.run(query, values, function(err) {
     //     if (err) return console.log(err)
@@ -43,13 +43,13 @@ db.serialize(function() {
 
     //Consultar dados na tabela
 
-    // db.all(`SELECT * FROM ideas`, function(err, rows) {
-    //     if (err) return console.log(err)
+    db.all(`SELECT * FROM ideas`, function(err, rows) {
+        if (err) return console.log(err)
 
-    //     console.log(rows)
-    // })
+        console.log(rows)
+    })
 
-    // Deletar um dado da tabela
+    ///Deletar um dado da tabela
     // db.run(`DELETE FROM ideas WHERE id = ?`, [1], function(err) {
     //     if (err) return console.log(err)
 
--- public/scripts.js
+++ public/scripts.js
@@ -15,3 +15,27 @@ function onOff() {
 }
 
 
+function checkFields(event) {
+    const valuesToCheck = [
+        "title",
+        "category",
+        "image",
+        "description",
+        "link",
+    ]
+
+
+    const isEmpty = valuesToCheck.find(function(value) {
+        const checkIfIsString = typeof event.target[value].value === "string"
+        const checkIsEmpty = !event.target[value].value.trim()
+
+        if(checkIfIsString && checkIsEmpty) {
+            return true
+        }
+    })
+
+    if(isEmpty) {
+        event.preventDefault()
+        alert("Por favor, preencha todos os campos!")
+    }
+}
